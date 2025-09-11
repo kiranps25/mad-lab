81cf1532eb0e1d18ff25ef5e9ade7adff9c992ca
kiranps25 <chn24mc066@ceconline.edu>
2025-09-11 15:34:33 +0530

    Create activityOne.java

---
+++ cycle3/Options Menu/activityOne.java
@@ -0,0 +1,12 @@
+package com.example.optionsmenu;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activityOne extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
